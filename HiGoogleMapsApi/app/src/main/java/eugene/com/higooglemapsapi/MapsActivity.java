92f3bd1c46d3f7d8d3a546247bf37ad5d42457ef
Eugene Lezov <ELezov@stud.kantiana.ru>
2017-02-08 01:17:20 +0200

    Add HiGoogleMapsApi
    this is my first project with Google Maps Api

---
+++ HiGoogleMapsApi/app/src/main/java/eugene/com/higooglemapsapi/MapsActivity.java
@@ -0,0 +1,100 @@
+package eugene.com.higooglemapsapi;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import static eugene.com.higooglemapsapi.R.id.map;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(map);
+        mapFragment.getMapAsync(this);
+        mMap = mapFragment.getMap();
+        if (mMap == null) {
+            finish();
+            return;
+        }
+
+
+        init();
+
+
+
+
+    }
+
+    public void init() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            return;
+        }
+        else mMap.setMyLocationEnabled(true);
+
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setZoomGesturesEnabled(true);
+
+    }
+
+
+    public void onClickTest(View view) {
+        int type;
+        type=mMap.getMapType();
+        switch (type){
+            case 0:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                break;
+            case 1:
+                mMap.setMapType(GoogleMap.MAP_TYPE_SATELLITE);
+                break;
+            case 2:
+                mMap.setMapType(GoogleMap.MAP_TYPE_TERRAIN);
+                break;
+            case 3:
+                mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+                break;
+            case 4:
+                mMap.setMapType(GoogleMap.MAP_TYPE_NONE);
+                break;
+        }
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        LatLng kant = new LatLng(54.706908, 20.512114);
+
+        MarkerOptions myMarker=new MarkerOptions().position(kant).title("Кафедральный собор");
+        myMarker.flat(true);
+        myMarker.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN));
+
+        mMap.addMarker(myMarker);
+
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom (kant, 16);
+        mMap.animateCamera(cameraUpdate);
+    }
+}
